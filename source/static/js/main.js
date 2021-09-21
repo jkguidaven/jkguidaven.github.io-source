fef2374ad7450f9918aebbe93035657abe4b86f5
James Kenneth A. Guidaven <jkguidaven@gmail.com>
2021-09-21 20:31:10 +0800

    Add minor UI animation and change some information

--- source/static/js/main.js
+++ source/static/js/main.js
@@ -21,4 +21,40 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
   }
+
+  document.addEventListener("scroll", () => {
+    animateSkillsetBox();
+    animateAffiliationBox();
+  });
 });
+
+function animateSkillsetBox() {
+  /*
+   * This will slightly slide the about box above by 150px when the
+   * viewport is getting closer to the element.
+   */
+  const MAX_OFFSET = 150;
+  const aboutBox = document.querySelector("#about-box");
+  const bottom = aboutBox.offsetTop + aboutBox.offsetHeight;
+  const top = window.scrollY + window.innerHeight;
+  const offset = bottom - top + MAX_OFFSET;
+
+  const margin = Math.min(MAX_OFFSET - offset, MAX_OFFSET);
+  document.querySelector(".skillset-content").style.marginTop =
+    -1 * margin + "px";
+
+  /*
+   * This will fadely show or hide the role icons
+   */
+  document.querySelectorAll(".role-icon").forEach((icon) => {
+    if (margin === MAX_OFFSET) {
+      icon.style.opacity = 1;
+      icon.style.filter = "sepia(60%)";
+    } else {
+      icon.style.opacity = 0;
+      icon.style.filter = "sepia(0)";
+    }
+  });
+}
+
+function animateAffiliationBox() {}
